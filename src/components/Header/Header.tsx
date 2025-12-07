f5c0d3032bcc44e12f89fb40fc1ee2ff97fca08c
Sayantan Pachal <sayantanpachal006@gmail.com>
2025-12-07 12:07:38 +0530

    update

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { FaUserCheck } from "react-icons/fa6";
+import { MdDarkMode } from "react-icons/md";
+
+function Header() {
+  return (
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+      <div className="flex items-center w-full md:2/3">
+        <Link href="/" className="font-black text-[#F27405]">
+          Hotelzz
+        </Link>
+
+        <ul className="flex items-center ml-5">
+          <li className="flex items-center">
+            <Link href="/auth">
+              <FaUserCheck className="cursor-pointer" />
+            </Link>
+          </li>
+          <li className="ml-2">
+            <MdDarkMode className="cursor-pointer" />
+          </li>
+        </ul>
+      </div>
+
+      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/rooms">Rooms</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
